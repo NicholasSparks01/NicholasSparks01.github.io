e2f3ada1ddddff64f0872d9fb3e7ec42f66816e1
NicholasSparks01 <93989654+NicholasSparks01@users.noreply.github.com>
2023-04-03 12:19:03 -0400

    Loading Screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-/*
-	Astral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
+document.getElementById("preloadPage");
+setTimeout(function() {
+    document.getElementById("preloadPage").style.display = "none";
+  }, 4000);
 
 (function($) {
 
